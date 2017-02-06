38eaa97d46e777868d9b225228a4ca470d247192
RayFoulk <rfoulk@gmail.com>
2017-02-06 14:22:36 -0500

    instruction set improvements and added rotate function to barrel shifter

--- Assembly/startup.s
+++ Assembly/startup.s
@@ -1,3 +1,5 @@
+;; Original Instruction Set
+
 start:  shr 6   ; zero out high char, will clear carry & compare
         oc0 0   ; set psel octet to return stack pointer
         oc1 1   ; set dsel octet to first ram bank
@@ -18,6 +20,23 @@ start:  shr 6   ; zero out high char, will clear carry & compare
         shl 6   ; zero out the accumulator
         shl 6
         a2b     ; zero out the operand register
+
+;; Revised Instruction Set
+
+start:  shl 6   ; pull up zero char for clearing carry & compare
+        lol 0   ; set psel octet to return stack pointer
+        lol 1   ; set dsel octet to first ram bank
+        mtx mcs ; initialize the mcs register
+        lol 7   ; load octal 7777 into accumulator
+        lol 7
+        lol 7
+        lol 7
+        mtx ptr ; initialize the return stack pointer to 7777
+        lol 5   ; change accumulator to 7775
+        rol     ; accumulator is now 7757
+        rol     ; accumulator is now 7577
+        inc psel ; increment psel to select data stack
+        mtx mcs ; initialize the data stack pointer to 7577
         
         
         
